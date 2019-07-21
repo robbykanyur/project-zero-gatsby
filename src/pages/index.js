2befb76307a48aa9638ed21303f21248925475df
Robert Kanyur <robert@kanyur.com>
2019-07-20 17:16:27 -0700

    Set project defaults

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import indexStyles from "./index.module.css"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <>
+    <Helmet>
+      <title>Home - Project Zero</title>
+    </Helmet>
+
+    <Layout>
+      <>
+        <header>
+
+        </header>
+      </>
+    </Layout>
+  </>
 )
 
 export default IndexPage
