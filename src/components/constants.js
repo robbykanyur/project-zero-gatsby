81f7266a48aa1b0552ff2169450d995e2e632e54
Robert Kanyur <robert@kanyur.com>
2019-07-31 10:32:21 -0700

    Opengraph meta tags

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,1 +1,2 @@
 export const API = "https://zero-api.elllipse.com/api/v1/"
+export const SiteRoot = "https://zero.elllipse.com/"
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,21 @@
 import React from "react"
+import Helmet from "react-helmet"
+
+import openGraph from "../images/open-graph.jpg"
 
 import "./layout.css"
 import "./fonts.css"
 
-const Layout = ({ children }) => {
-  return <div>{children}</div>
-}
+const Layout = ({ children }) => (
+  <>
+    <Helmet>
+      />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:image" content={openGraph} />
+      <meta property="og:image" content={openGraph} />
+    </Helmet>
+    <div>{children}</div>
+  </>
+)
 
 export default Layout
