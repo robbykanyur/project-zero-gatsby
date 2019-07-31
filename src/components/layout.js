80e95ce9162bbbb65b32b2c70bc255ec51060271
Robert Kanyur <robert@kanyur.com>
2019-07-31 17:43:02 +0000

    Updated meta tags

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,9 +10,13 @@ const Layout = ({ children }) => (
   <>
     <Helmet>
       />
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="Project Zero" />
       <meta name="twitter:image" content={openGraph} />
+      <meta name="og:type" content="website" />
+      <meta name="og:title" content="Project Zero" />
       <meta property="og:image" content={openGraph} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
     </Helmet>
     <div>{children}</div>
   </>
