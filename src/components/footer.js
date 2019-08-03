34b0f48c890f1303b664cb852de983b35a65b685
Robert Kanyur <robert@kanyur.com>
2019-08-02 18:02:01 -0700

    Prismic fields on home page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,29 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import footerStyles from "./footer.module.css"
 
-const Footer = ({ children }) => (
-  <div className={footerStyles.container}>
-    <div className={footerStyles.wrapper}>
-      <p className={footerStyles.paragraph}>
-        101 Colonnade St. #302 // Lynchburg, VA 24502
-      </p>
-      <div className={footerStyles.linksWrapper}>
-        <Link className={footerStyles.link} to="/">
-          About
-        </Link>
-        <Link className={footerStyles.link} to="/our-team">
-          Our Team
-        </Link>
-        <Link className={footerStyles.link} to="/contact">
-          Contact
-        </Link>
-        <Link className={footerStyles.link} to="/donate">
-          Donate
-        </Link>
+const Footer = props => {
+  const d = useStaticQuery(graphql`
+    {
+      prismic {
+        allComponent_footers {
+          edges {
+            node {
+              address_line_1
+              address_line_2
+            }
+          }
+        }
+      }
+    }
+  `)
+  const data = d.prismic.allComponent_footers.edges.slice(0, 1).pop().node
+  if (!data) return null
+
+  return (
+    <div className={footerStyles.container}>
+      <div className={footerStyles.wrapper}>
+        <p className={footerStyles.paragraph}>
+          {data.address_line_1} &#47;&#47; {data.address_line_2}
+        </p>
+        <div className={footerStyles.linksWrapper}>
+          <Link className={footerStyles.link} to="/">
+            About
+          </Link>
+          <Link className={footerStyles.link} to="/our-team">
+            Our Team
+          </Link>
+          <Link className={footerStyles.link} to="/contact">
+            Contact
+          </Link>
+          <Link className={footerStyles.link} to="/donate">
+            Donate
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react"
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { Helmet } from "react-helmet"
+import { RichText } from "prismic-reactjs"
 
 import indexStyles from "./index.module.css"
 
@@ -15,187 +17,233 @@ import zigzag from "../images/tex-zebra.png"
 import polaroid from "../images/home-polaroid.png"
 import squares from "../images/home-squares.png"
 import paint from "../images/tex-wall.png"
-import caleb from "../images/profile-caleb.jpg"
 import logo from "../images/base-logo.png"
 
-class IndexPage extends Component {
-  render() {
-    return (
-      <>
-        <Helmet>
-          <title>Home - Project Zero</title>
-        </Helmet>
+const IndexPage = () => {
+  const d = useStaticQuery(graphql`
+    {
+      prismic {
+        allPage_homes {
+          edges {
+            node {
+              cost_per_month
+              cta_text
+              intro_heading
+              intro_paragraph
+              lead_button
+              lead_heading
+              lead_paragraph
+              mission_heading
+              mission_paragraph
+              page_title
+              problem_description_1
+              problem_description_2
+              problem_description_3
+              problem_description_4
+              problem_footnotes
+              problem_value_1
+              problem_value_2
+              problem_value_3
+              problem_value_4
+              quote_attrib
+              quote_image
+              quote_text
+            }
+          }
+        }
+      }
+    }
+  `)
+  const data = d.prismic.allPage_homes.edges.slice(0, 1).pop().node
+  if (!data) return null
 
-        <Layout>
-          <>
-            <Header logoSrc={logo} />
-            <Block backgroundImage={hero}>
-              <div className="animated fadeInUp">
-                <div className={indexStyles.hero}>
-                  <h1 className="is-1 is-centered">
-                    Project Zero works to ensure every resident of Lynchburg,
-                    Virginia has a roof over their head.
-                  </h1>
-                  <p className="is-centered is-subtitle">
-                    We believe that Lynchburg's 100+ homeless residents are
-                    family, and that a family cares for its own.
-                  </p>
-                  <div className="is-centered-text">
-                    <Button link="/donate" text="Get Involved" width="193px" />
-                  </div>
+  return (
+    <>
+      <Helmet>
+        <title>{data.page_title}</title>
+      </Helmet>
+
+      <Layout>
+        <>
+          <Header logoSrc={logo} />
+          <Block backgroundImage={hero}>
+            <div className="animated fadeInUp">
+              <div className={indexStyles.hero}>
+                <h1 className="is-1 is-centered">{data.lead_heading}</h1>
+                <div
+                  className={indexStyles.wrapLead + " is-centered is-subtitle"}
+                >
+                  {RichText.render(data.lead_paragraph)}
+                </div>
+                <div className="is-centered-text">
+                  <Button
+                    link="/donate"
+                    text={data.lead_button}
+                    width="193px"
+                  />
                 </div>
               </div>
-            </Block>
-            <div id="about" name="about"></div>
-            <Block>
-              <div className={indexStyles.everyPerson}>
-                <h2 className="is-1 is-centered is-serif">
-                  Until every person has a home.
-                </h2>
-                <p className="is-centered is-subtitle">
-                  More than a hundred residents of Lynchburg, Virginia are
-                  without homes, sleeping on streets and under bridges. More
-                  than just residents, <strong>they are our neighbors.</strong>
-                </p>
+            </div>
+          </Block>
+          <div id="about" name="about"></div>
+          <Block>
+            <div className={indexStyles.everyPerson}>
+              <h2 className="is-1 is-centered is-serif">
+                {data.intro_heading}
+              </h2>
+              <div
+                className={
+                  indexStyles.wrapEveryPerson + " is-centered is-subtitle"
+                }
+              >
+                {RichText.render(data.intro_paragraph)}
               </div>
-            </Block>
-            <Block
-              backgroundColor="#f2f2f2"
-              pattern={zigzag}
-              tileOpacity="0.22"
-              containerPaddingBottom="300px"
-            >
-              <div className={indexStyles.coordinated}>
-                <h2 className="is-2 is-centered is-serif">
-                  Project Zero is a coordinated effort to deliver the resources
-                  of our community to those in need.
-                </h2>
-                <p className="is-centered is-subtitle">
-                  Through partnerships with churches and other non-profit
-                  organizations, we are striving to end homelessness in the city
-                  of Lynchburg.
-                </p>
-                <div className={indexStyles.polaroid}>
-                  <div className={indexStyles.polaroidContainer}>
-                    <div className={indexStyles.polaroidWrapper}>
-                      <img
-                        src={polaroid}
-                        width="100%"
-                        alt="Homeless neighbors in Lynchburg"
-                      />
-                    </div>
-                  </div>
-                </div>
+            </div>
+          </Block>
+          <Block
+            backgroundColor="#f2f2f2"
+            pattern={zigzag}
+            tileOpacity="0.22"
+            containerPaddingBottom="300px"
+          >
+            <div className={indexStyles.coordinated}>
+              <h2 className="is-2 is-centered is-serif">
+                {data.mission_heading}
+              </h2>
+              <div
+                className={
+                  indexStyles.wrapCoordinated + " is-centered is-subtitle"
+                }
+              >
+                {RichText.render(data.mission_paragraph)}
               </div>
-            </Block>
-            <Block>
-              <div className={indexStyles.costContainer}>
-                <div className={indexStyles.costWrapper}>
-                  <div className={indexStyles.costLeft}>
-                    <p>
-                      It only costs{" "}
-                      <span className={indexStyles.costHighlight}>
-                        $550 per month
-                        <br />
-                      </span>{" "}
-                      to provide{" "}
-                      <span className={indexStyles.costHighlight}>
-                        housing, clothing,
-                        <br /> food,
-                      </span>{" "}
-                      and{" "}
-                      <span className={indexStyles.costHighlight}>
-                        healthcare
-                      </span>{" "}
-                      for a <br />
-                      homeless neighbor.
-                    </p>
-                  </div>
-                  <div className={indexStyles.costRight}>
+              <div className={indexStyles.polaroid}>
+                <div className={indexStyles.polaroidContainer}>
+                  <div className={indexStyles.polaroidWrapper}>
                     <img
-                      className={indexStyles.costSquares}
-                      src={squares}
+                      src={polaroid}
                       width="100%"
-                      alt="Homeless neighbors sleeping on the streets of Lynchburg"
+                      alt="Homeless neighbors in Lynchburg"
                     />
-                    <div className={indexStyles.costLine}></div>
                   </div>
                 </div>
               </div>
-            </Block>
-            <Block
-              backgroundColor="#3F4F63"
-              pattern={paint}
-              tileOpacity="0.5"
-              containerPaddingBottom="0"
-            >
-              <div className={indexStyles.problem}>
-                <h2>The Problem</h2>
-                <div className={indexStyles.problemRow}>
-                  <div className={indexStyles.problemItem}>
-                    <h3 className={indexStyles.problemItemValue}>23.1%</h3>
-                    <p className={indexStyles.problemItemDescription}>
-                      poverty rate*
-                    </p>
-                  </div>
-                  <div className={indexStyles.problemItem}>
-                    <h3 className={indexStyles.problemItemValue}>112</h3>
-                    <p className={indexStyles.problemItemDescription}>
-                      homeless individuals
-                    </p>
-                  </div>
-                  <div className={indexStyles.problemItem}>
-                    <h3 className={indexStyles.problemItemValue}>~300</h3>
-                    <p className={indexStyles.problemItemDescription}>
-                      religious organizations**
-                    </p>
-                  </div>
-                  <div className={indexStyles.problemItem}>
-                    <h3 className={indexStyles.problemItemValue}>$39,589</h3>
-                    <p className={indexStyles.problemItemDescription}>
-                      median household income
-                    </p>
-                  </div>
+            </div>
+          </Block>
+          <Block>
+            <div className={indexStyles.costContainer}>
+              <div className={indexStyles.costWrapper}>
+                <div className={indexStyles.costLeft}>
+                  <p>
+                    It only costs{" "}
+                    <span className={indexStyles.costHighlight}>
+                      {data.cost_per_month} per month
+                      <br />
+                    </span>{" "}
+                    to provide{" "}
+                    <span className={indexStyles.costHighlight}>
+                      housing, clothing,
+                      <br /> food,
+                    </span>{" "}
+                    and{" "}
+                    <span className={indexStyles.costHighlight}>
+                      healthcare
+                    </span>{" "}
+                    for a <br />
+                    homeless neighbor.
+                  </p>
                 </div>
-                <div className={indexStyles.problemFootnotes}>
-                  <p>*9.6% higher than the national average</p>
-                  <p>**In the city of Lynchburg and surrounding counties</p>
+                <div className={indexStyles.costRight}>
+                  <img
+                    className={indexStyles.costSquares}
+                    src={squares}
+                    width="100%"
+                    alt="Homeless neighbors sleeping on the streets of Lynchburg"
+                  />
+                  <div className={indexStyles.costLine}></div>
                 </div>
               </div>
-            </Block>
-            <Block>
-              <div className={indexStyles.quote}>
-                <div className={indexStyles.quoteWrapper}>
-                  <div className={indexStyles.quotePhoto}>
-                    <img src={caleb} width="100%" alt="Caleb Fitzpatrick" />
-                  </div>
-                  <div className={indexStyles.quoteText}>
-                    <p>
-                      &ldquo;Ending homelessness in this city is a much more
-                      attainable goal than most people realize. With your help,
-                      we can accomplish this.&rdquo;
-                    </p>
-                  </div>
-                  <div className={indexStyles.quoteAttrib}>
-                    <p>Caleb Fitzpatrick, Co-Founder</p>
-                  </div>
+            </div>
+          </Block>
+          <Block
+            backgroundColor="#3F4F63"
+            pattern={paint}
+            tileOpacity="0.5"
+            containerPaddingBottom="0"
+          >
+            <div className={indexStyles.problem}>
+              <h2>The Problem</h2>
+              <div className={indexStyles.problemRow}>
+                <div className={indexStyles.problemItem}>
+                  <h3 className={indexStyles.problemItemValue}>
+                    {data.problem_value_1}
+                  </h3>
+                  <p className={indexStyles.problemItemDescription}>
+                    {data.problem_description_1}
+                  </p>
+                </div>
+                <div className={indexStyles.problemItem}>
+                  <h3 className={indexStyles.problemItemValue}>
+                    {data.problem_value_2}
+                  </h3>
+                  <p className={indexStyles.problemItemDescription}>
+                    {data.problem_description_2}
+                  </p>
+                </div>
+                <div className={indexStyles.problemItem}>
+                  <h3 className={indexStyles.problemItemValue}>
+                    {data.problem_value_3}
+                  </h3>
+                  <p className={indexStyles.problemItemDescription}>
+                    {data.problem_description_3}
+                  </p>
+                </div>
+                <div className={indexStyles.problemItem}>
+                  <h3 className={indexStyles.problemItemValue}>
+                    {data.problem_value_4}
+                  </h3>
+                  <p className={indexStyles.problemItemDescription}>
+                    {data.problem_description_4}
+                  </p>
                 </div>
               </div>
-            </Block>
-            <Cta
-              linkOneText="Donate"
-              linkOneHref="/donate"
-              toggleModal={true}
-              linkTwoText="Serve"
-              linkTwoHref="/serve"
-              textContent="Help us eradicate homelessness in Lynchburg."
-            />
-            <Footer></Footer>
-          </>
-        </Layout>
-      </>
-    )
-  }
+              <div className={indexStyles.problemFootnotes}>
+                {RichText.render(data.problem_footnotes)}
+              </div>
+            </div>
+          </Block>
+          <Block>
+            <div className={indexStyles.quote}>
+              <div className={indexStyles.quoteWrapper}>
+                <div className={indexStyles.quotePhoto}>
+                  <img
+                    src={data.quote_image.url}
+                    width="100%"
+                    alt={data.quote_image.alt}
+                  />
+                </div>
+                <div className={indexStyles.quoteText}>
+                  <p>{data.quote_text}</p>
+                </div>
+                <div className={indexStyles.quoteAttrib}>
+                  {data.quote_attrib}
+                </div>
+              </div>
+            </div>
+          </Block>
+          <Cta
+            linkOneText="Donate"
+            linkOneHref="/donate"
+            toggleModal={true}
+            linkTwoText="Serve"
+            linkTwoHref="/serve"
+            textContent={data.cta_text}
+          />
+          <Footer></Footer>
+        </>
+      </Layout>
+    </>
+  )
 }
 
 export default IndexPage
