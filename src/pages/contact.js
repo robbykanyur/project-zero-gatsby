80f0475d13c9b3deffedb9481279d716d827c128
Robert Kanyur <robert@kanyur.com>
2019-09-25 00:52:08 -0700

    Fixed graphql query syntax on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react"
 import { Helmet } from "react-helmet"
 import MaskedInput from "react-text-mask"
-import { navigate, graphql, StaticQuery } from "gatsby"
+import { navigate, graphql } from "gatsby"
 import * as Constants from "../components/constants"
 import { RichText } from "prismic-reactjs"
-import { withPreview } from "gatsby-source-prismic-graphql"
-
 import contactStyles from "./contact.module.css"
 
 import Layout from "../components/layout"
@@ -107,155 +105,117 @@ class ContactPage extends Component {
     const formErrors = this.state.errors.map((error, key) => (
       <li key={error.id}>{error.message}</li>
     ))
+    const data = this.props.data.prismic.allPage_contacts.edges
+      .slice(0, 1)
+      .pop().node
     return (
-      <StaticQuery
-        query={query}
-        render={withPreview(
-          data => (
-            <>
-              <Helmet>
-                <title>
-                  {
-                    data.prismic.allPage_contacts.edges.slice(0, 1).pop().node
-                      .page_title
-                  }
-                </title>
-              </Helmet>
-              <Layout>
-                <>
-                  <Hero
-                    title={
-                      data.prismic.allPage_contacts.edges.slice(0, 1).pop().node
-                        .header_text
-                    }
-                    photo={heroPhoto}
-                  ></Hero>
-                  <Block>
-                    <div
-                      className={contactStyles.lead + " is-1 is-centered-text"}
-                    >
-                      {RichText.render(
-                        data.prismic.allPage_contacts.edges.slice(0, 1).pop()
-                          .node.lead_paragraph
-                      )}
-                    </div>
-                    <div className={contactStyles.captureForm}>
-                      <form
-                        className="formControl"
-                        id="teamCapture"
-                        name="teamCapture"
-                      >
-                        <div className="control">
-                          <input
-                            type="text"
-                            placeholder="Your Name"
-                            onChange={e => this.handleNameChange(e)}
-                          />
-                        </div>
-                        <div className="control">
-                          <input
-                            type="email"
-                            placeholder="Email Address"
-                            onChange={e => this.handleEmailChange(e)}
-                          />
-                        </div>
-                        <div className="control">
-                          <MaskedInput
-                            type="phone"
-                            placeholder="Phone Number"
-                            mask={mask}
-                            placeholderChar="&nbsp;"
-                            onChange={e => this.handlePhoneChange(e)}
-                          />
-                        </div>
-                        <div className="control">
-                          <textarea
-                            placeholder="Your Message"
-                            onChange={e => this.handleMessageChange(e)}
-                          ></textarea>
-                        </div>
-                        <div className="captcha">
-                          <input
-                            type="checkbox"
-                            onChange={e => this.handleCaptchaChange(e)}
-                          />{" "}
-                          Check this box if you are a human.
-                        </div>
-                        <div className="formErrors">{formErrors}</div>
-                        <div className="control">
-                          <Button
-                            link="/"
-                            text="Send Message"
-                            width="201px"
-                            onClick={e => this.handleFormSubmit(e)}
-                          />
-                        </div>
-                      </form>
-                    </div>
-                  </Block>
-                  <Block containerPaddingTop="0" containerPaddingBottom="0">
-                    <div className={contactStyles.detailsContainer}>
-                      <div className={contactStyles.detailsWrapper}>
-                        <div className={contactStyles.detailsLeft}>
-                          <h3>Project Zero</h3>
-                          <p>
-                            {
-                              data.prismic.allPage_contacts.edges
-                                .slice(0, 1)
-                                .pop().node.address_line_1
-                            }
-                            <br />
-                            {
-                              data.prismic.allPage_contacts.edges
-                                .slice(0, 1)
-                                .pop().node.address_line_2
-                            }
-                          </p>
-                          <hr />
-                          <p>
-                            {
-                              data.prismic.allPage_contacts.edges
-                                .slice(0, 1)
-                                .pop().node.contact_email
-                            }
-                          </p>
-                        </div>
-                        <div className={contactStyles.detailsRight}>
-                          <img
-                            src={squares}
-                            width="100%"
-                            alt="Buildings in Lynchburg"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </Block>
-                  <Cta
-                    linkOneText="Donate"
-                    linkOneHref="/donate"
-                    toggleModal={true}
-                    linkTwoText="Serve"
-                    linkTwoHref="/serve"
-                    textContent={
-                      data.prismic.allPage_contacts.edges.slice(0, 1).pop().node
-                        .cta_text
-                    }
-                  />
-                  <Footer></Footer>
-                </>
-              </Layout>
-            </>
-          ),
-          query
-        )}
-      />
+      <>
+        <Helmet>
+          <title>{data.page_title}</title>
+        </Helmet>
+        <Layout>
+          <>
+            <Hero title={data.header_text} photo={heroPhoto}></Hero>
+            <Block>
+              <div className={contactStyles.lead + " is-1 is-centered-text"}>
+                {RichText.render(data.lead_paragraph)}
+              </div>
+              <div className={contactStyles.captureForm}>
+                <form
+                  className="formControl"
+                  id="teamCapture"
+                  name="teamCapture"
+                >
+                  <div className="control">
+                    <input
+                      type="text"
+                      placeholder="Your Name"
+                      onChange={e => this.handleNameChange(e)}
+                    />
+                  </div>
+                  <div className="control">
+                    <input
+                      type="email"
+                      placeholder="Email Address"
+                      onChange={e => this.handleEmailChange(e)}
+                    />
+                  </div>
+                  <div className="control">
+                    <MaskedInput
+                      type="phone"
+                      placeholder="Phone Number"
+                      mask={mask}
+                      placeholderChar="&nbsp;"
+                      onChange={e => this.handlePhoneChange(e)}
+                    />
+                  </div>
+                  <div className="control">
+                    <textarea
+                      placeholder="Your Message"
+                      onChange={e => this.handleMessageChange(e)}
+                    ></textarea>
+                  </div>
+                  <div className="captcha">
+                    <input
+                      type="checkbox"
+                      onChange={e => this.handleCaptchaChange(e)}
+                    />{" "}
+                    Check this box if you are a human.
+                  </div>
+                  <div className="formErrors">{formErrors}</div>
+                  <div className="control">
+                    <Button
+                      link="/"
+                      text="Send Message"
+                      width="201px"
+                      onClick={e => this.handleFormSubmit(e)}
+                    />
+                  </div>
+                </form>
+              </div>
+            </Block>
+            <Block containerPaddingTop="0" containerPaddingBottom="0">
+              <div className={contactStyles.detailsContainer}>
+                <div className={contactStyles.detailsWrapper}>
+                  <div className={contactStyles.detailsLeft}>
+                    <h3>Project Zero</h3>
+                    <p>
+                      {data.address_line_1}
+                      <br />
+                      {data.address_line_2}
+                    </p>
+                    <hr />
+                    <p>{data.contact_email}</p>
+                  </div>
+                  <div className={contactStyles.detailsRight}>
+                    <img
+                      src={squares}
+                      width="100%"
+                      alt="Buildings in Lynchburg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </Block>
+            <Cta
+              linkOneText="Donate"
+              linkOneHref="/donate"
+              toggleModal={true}
+              linkTwoText="Serve"
+              linkTwoHref="/serve"
+              textContent={data.cta_text}
+            />
+            <Footer></Footer>
+          </>
+        </Layout>
+      </>
     )
   }
 }
 
 export default ContactPage
 
-const query = graphql`
+export const query = graphql`
   {
     prismic {
       allPage_contacts {
