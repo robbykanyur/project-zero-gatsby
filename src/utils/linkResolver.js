03e165a100299d04e7a0a4e689adf99d7e238f7c
Robert Kanyur <robert@kanyur.com>
2019-09-26 16:43:55 -0700

    Updated link resolver with UIDs

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -4,10 +4,10 @@ module.exports = {
     if (doc.type === "page_team") return "/our-team"
     if (doc.type === "page_contact") return "/contact"
     if (doc.type === "page_donate") return "/donate"
-    if (doc.type === "page_serve") return "/serve"
-    if (doc.type === "page_thanks_contact") return "/thank-you-contact"
-    if (doc.type === "page_thanks_donation") return "/thank-you-donation"
-    if (doc.type === "page_thanks_serve") return "/thank-you-serve"
+    if (doc.type === "serve") return "/serve"
+    if (doc.uid === "thank-you-contact") return "/thank-you-contact"
+    if (doc.uid === "thank-you-donation") return "/thank-you-donation"
+    if (doc.uid === "thank-you-serve") return "/thank-you-serve"
     return "/"
   },
 }
